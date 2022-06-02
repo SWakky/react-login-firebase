18c00e21a99e2892e5d8234ce0bdbb70873a3b5b
SWakky <shuta.sakawaki1028@gmail.com>
2022-06-03 06:28:22 +0900

    ok

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import 'firebase/compat/firestore';
 // import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM6vStPKhYPhsi_9zi8NW6v0v01F-SUU4",
-  authDomain: "fir-login-9d49f.firebaseapp.com",
-  projectId: "fir-login-9d49f",
-  storageBucket: "fir-login-9d49f.appspot.com",
-  messagingSenderId: "66691081478",
-  appId: "1:66691081478:web:929a3925b193f5f1992a87"
+  apiKey: "***",
+  authDomain: "***",
+  projectId: "***",
+  storageBucket: "***",
+  messagingSenderId: "***",
+  appId: "***"
 };
 
 firebase.initializeApp(firebaseConfig);
